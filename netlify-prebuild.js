744f752e89bbc764db9c92003f46038a21784ec1
Mehul <mehulshrivastava1011@gmail.com>
2023-06-29 21:09:18 +0530

    Environment update for prod

--- netlify-prebuild.js
+++ netlify-prebuild.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 // Read the environment variables from Netlify
 const apiKey = process.env.FIREBASE_API_KEY; // Replace with your own variable names
+const production = true;
 const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
@@ -20,15 +21,17 @@ const outputFile = "./src/environments/environment.prod.ts";
 const options = {
   files: filePath,
   from: [
-    /REPLACE_WITH_ENVIRONMENT_API_KEY/,
-    /REPLACE_WITH_AUTH_DOMAIN/,
-    /REPLACE_WITH_PROJECT_ID/,
-    /REPLACE_WITH_STORAGE_BUCKET/,
-    /REPLACE_WITH_MESSAGING_SENDER_ID/,
-    /REPLACE_WITH_APP_ID/,
-    /REPLACE_WITH_MEASUREMENT_ID/,
+    production,
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
   ],
   to: [
+    production,
     apiKey,
     authDomain,
     projectId,
