f70a37dbd8fd2343f4a59eb123b42e63114feaec
Mehul <mehulshrivastava1011@gmail.com>
2023-06-29 19:39:04 +0530

    Initial files for crash-store

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
-  },
+  production: true,
 };
